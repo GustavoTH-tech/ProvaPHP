2ca43f6705667443e2dd84f4480df8e454a0c22e
GustavoTH-tech <gustavofernandes4828@gmail.com>
2024-11-29 16:02:27 -0300

    provaPHP

---
+++ index.php
@@ -0,0 +1,69 @@
+<h2>
+    Receita:
+    <?php
+    echo"strogonoff de frango"
+    ?>
+</h2>
+<table border = "1">
+    <thead>
+        <tr>
+            <th>Ingredientes</th>
+            <th>Quantidade</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>peitos de frango cortados em cubos</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>sal</td>
+            <td>a gosto</td>
+        </tr>
+        <tr>
+            <td>cebola picada</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>colher de manteiga</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td> copo de mostarda</td>
+            <td>1/3</td>
+        </tr>
+        <tr>
+            <td>creme de leite</td>
+            <td>1 copo</td>
+        </tr>
+        <tr>
+            <td> dente de alho picado</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>pimenta-do-reino</td>
+            <td>a gosto</td>
+        </tr>
+        <tr>
+            <td>maionese</td>
+            <td>2 colheres (sopa)</td>
+        </tr>
+        <tr>
+            <td>ketchup</td>
+            <td>1/2 copo</td>
+        </tr>
+        <tr>
+            <td>cogumelos</td>
+            <td>1 copo</td>
+        </tr>
+        <tr>
+            <td>batata palha</td>
+            <td>a gosto</td>
+        </tr>
+    </tbody>
+</table>
+<h1>Nota: 10</h1>
+<script>
+    var.h1 = document.querySelector("h1");
+    h1.innerHTML = "nota: 10";
+</script>
